c9567f48b2ed8a62dac3fd5c05faf4f4f93a8689
Osvaldo <ojimenez@pacific.edu>
2023-03-07 18:14:34 -0800

    refactor: used a switch statement with default values

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const counterReducer = (state, action) => {
-    if (action.type === 'INCREMENT') {
-        return state + 1
-    } else if (action.type === 'DECREMENT') {
-        return state - 1
-    } else if (action.type === 'ZERO') {
-        return 0
+const counterReducer = (state = 0, action) => {
+    switch (action.type) {
+        case 'INCREMENT':
+            return state + 1
+        case 'DECREMENT':
+            return state - 1
+        case 'ZERO':
+            return 0
+        default: // if none of the above matches, code comes here
+            return state
     }
-
-    return state
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
