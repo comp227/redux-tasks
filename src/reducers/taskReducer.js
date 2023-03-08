ba6bc754103782b863767f4debfac759df739d0f
Osvaldo <ojimenez@pacific.edu>
2023-03-08 12:48:41 -0800

    feat: implemented toggle importance functionality for task reducer

--- src/reducers/taskReducer.js
+++ src/reducers/taskReducer.js
@@ -1,9 +1,21 @@
 const taskReducer = (state = [], action) => {
-    if (action.type === 'NEW_TASK') {
-        return state.concat(action.payload)
+    switch(action.type) {
+        case 'NEW_TASK':
+            return state.concat(action.payload)
+        case 'TOGGLE_IMPORTANCE': {
+            const id = action.payload.id
+            const taskToChange = state.find(t => t.id === id)
+            const changedTask = {
+                ...taskToChange,
+                important: !taskToChange.important
+            }
+            return state.map(task =>
+                task.id !== id ? task : changedTask
+            )
+        }
+        default:
+            return state
     }
-
-    return state
 }
 
 export default taskReducer
