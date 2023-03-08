53c4ba4b9d83f1ae593e1dd200001daddc4a5a1d
Osvaldo <ojimenez@pacific.edu>
2023-03-07 18:12:43 -0800

    feat: added counter reducer

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+const counterReducer = (state, action) => {
+    if (action.type === 'INCREMENT') {
+        return state + 1
+    } else if (action.type === 'DECREMENT') {
+        return state - 1
+    } else if (action.type === 'ZERO') {
+        return 0
+    }
+
+    return state
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<div>Hello World!</div>);
