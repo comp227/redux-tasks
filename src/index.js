42b83d323a28032a73bec268e894d03b43bb50d5
Osvaldo <ojimenez@pacific.edu>
2023-03-08 11:16:37 -0800

    feat: added redux package

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<div>Hello World!</div>);
+root.render(<App />);
