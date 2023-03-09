9627cf11f7e500a90792f35bf5ff27355b66b153
Osvaldo <ojimenez@pacific.edu>
2023-03-09 07:51:15 -0800

    feat: added radio buttons for important tasks

--- src/App.js
+++ src/App.js
@@ -2,10 +2,22 @@ import Tasks from "./components/Tasks";
 import NewTask from "./components/NewTask";
 
 const App = () => {
+    const filterSelected = (value) => {
+        console.log(value)
+    }
+
     return (
         <div>
-            <NewTask />
-            <Tasks />
+            <NewTask/>
+            <div>
+                <input type="radio" name="filter"
+                       onChange={() => filterSelected('ALL')}/>all
+                <input type="radio" name="filter"
+                       onChange={() => filterSelected('IMPORTANT')}/>important
+                <input type="radio" name="filter"
+                       onChange={() => filterSelected('UNIMPORTANT')}/>unimportant
+            </div>
+            <Tasks/>
         </div>
     )
 }
