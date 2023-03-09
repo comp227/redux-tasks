74861bfa3172ed1093fb78e06fd1805a86f52860
Osvaldo <ojimenez@pacific.edu>
2023-03-08 19:02:07 -0800

    refactor: created the new Tasks file

---
+++ src/components/Tasks.js
@@ -0,0 +1,32 @@
+import { useDispatch, useSelector } from 'react-redux'
+import { toggleImportanceOf } from '../reducers/taskReducer'
+
+const Task = ({ task, handleClick }) => {
+    return(
+        <li onClick={handleClick}>
+            {task.content}
+            <strong> {task.important ? 'important' : ''}</strong>
+        </li>
+    )
+}
+
+const Tasks = () => {
+    const dispatch = useDispatch()
+    const tasks = useSelector(state => state)
+
+    return(
+        <ul>
+            {tasks.map(task =>
+                <Task
+                    key={task.id}
+                    task={task}
+                    handleClick={() =>
+                        dispatch(toggleImportanceOf(task.id))
+                    }
+                />
+            )}
+        </ul>
+    )
+}
+
+export default Tasks
