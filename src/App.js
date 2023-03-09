a43eeb9afe1862b5cfc0ae27e78546a177121fd6
Osvaldo <ojimenez@pacific.edu>
2023-03-08 19:05:15 -0800

    refactor: setup the App component

--- src/App.js
+++ src/App.js
@@ -1,37 +1,11 @@
-import {createTask, toggleImportanceOf} from './reducers/taskReducer'
-import {useSelector, useDispatch} from 'react-redux'
+import Tasks from "./components/Tasks";
+import NewTask from "./components/NewTask";
 
 const App = () => {
-    const dispatch = useDispatch()
-    const tasks = useSelector(state => state)
-
-    const addTask = (event) => {
-        event.preventDefault()
-        const content = event.target.task.value
-        event.target.task.value = ''
-        dispatch(createTask(content))
-    }
-
-    const toggleImportance = (id) => {
-        dispatch(toggleImportanceOf(id))
-    }
-
     return (
         <div>
-            <form onSubmit={addTask}>
-                <input name="task"/>
-                <button type="submit">add</button>
-            </form>
-            <ul>
-                {tasks.map(task =>
-                    <li
-                        key={task.id}
-                        onClick={() => toggleImportance(task.id)}
-                    >
-                        {task.content} <strong>{task.important ? 'important' : ''}</strong>
-                    </li>
-                )}
-            </ul>
+            <NewTask />
+            <Tasks />
         </div>
     )
 }
