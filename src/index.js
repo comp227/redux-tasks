9f60079aedb6a4bc4a8bb4574323a263c08ce89a
Osvaldo <ojimenez@pacific.edu>
2023-03-10 13:58:38 -0800

    refactor: moved store creation in index out to its own file

--- src/index.js
+++ src/index.js
@@ -1,20 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {Provider} from 'react-redux'
-import {configureStore} from '@reduxjs/toolkit'
-
 import App from './App'
-import taskReducer from './reducers/taskReducer'
-import filterReducer from "./reducers/filterReducer";
+import store from "./store";
 
-const store = configureStore({
-    reducer: {
-        tasks: taskReducer,
-        filter: filterReducer
-    }
-})
-
-console.log(store.getState())
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <Provider store={store}>
---
+++ src/store.js
@@ -0,0 +1,13 @@
+import { configureStore } from '@reduxjs/toolkit'
+
+import taskReducer from './reducers/taskReducer'
+import filterReducer from './reducers/filterReducer'
+
+const store = configureStore({
+    reducer: {
+        tasks: taskReducer,
+        filter: filterReducer
+    }
+})
+
+export default store
