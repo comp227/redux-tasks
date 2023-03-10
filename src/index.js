435f62eb9de8929acc94856362fdcae32ab5e77a
Osvaldo <ojimenez@pacific.edu>
2023-03-09 19:20:28 -0800

    feat: used configureStore instead of createStore

--- src/index.js
+++ src/index.js
@@ -1,23 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createStore, combineReducers } from 'redux'
 import { Provider } from 'react-redux'
+import { configureStore } from '@reduxjs/toolkit'
 
 import App from './App'
 import taskReducer from './reducers/taskReducer'
 import filterReducer from "./reducers/filterReducer";
 
-const reducer = combineReducers({
-    tasks: taskReducer,
-    filter: filterReducer
+const store = configureStore({
+    reducer: {
+        tasks: taskReducer,
+        filter: filterReducer
+    }
 })
 
-const store = createStore(reducer)
 console.log(store.getState())
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <Provider store={store}>
         <App />
-        {/*<div/>*/}
     </Provider>
 )
